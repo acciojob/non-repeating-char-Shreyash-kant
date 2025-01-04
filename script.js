4de76414718bdee48b5d5a360c467a1d0eee2dbd
Shreyash Kant <108665847+Shreyash-kant@users.noreply.github.com>
2025-01-04 20:11:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str.length===0)return null;
+	const freq = [];
+	for(let i=0;i<26;++i)freq.push(0);
+	for(let i=0;i<str.length;++i)freq[str.charCodeAt(i)-97]+=1;
+	for(let i=0;i<26;++i){
+		if(freq[i]===1)return String.fromCharCode(i+97);
+	}
+	return null;
 }
-const input = prompt("Enter a string");
+// const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+  
